cf2a24b6ca263000781d0df2ac114a7e12589713
grishingrishin <grishingrishin@yahoo.com>
2023-12-21 23:13:40 +0300

    Removed condition

--- src/utils/helpers/injectParams.js
+++ src/utils/helpers/injectParams.js
@@ -1,7 +1,5 @@
 export function injectParams(url, params) {
 	Object.keys(params).forEach(key => {
-		if (params[key]) {
-			url.searchParams.append(key, params[key]);
-		}
+		url.searchParams.append(key, params[key]);
 	});
 }
