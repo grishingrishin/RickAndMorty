95b46770c724e75c43513bc0384519287c15a42e
grishingrishin <grishingrishin@yahoo.com>
2023-12-24 15:48:16 +0300

    Added new environment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import path from 'path';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-		vue(),
-	],
+	plugins: [vue()],
 	resolve: {
 		alias: [
 			{
@@ -15,4 +13,7 @@ export default defineConfig({
 			},
 		],
 	},
-})
+	test: {
+		environment: 'happy-dom',
+	},
+});
