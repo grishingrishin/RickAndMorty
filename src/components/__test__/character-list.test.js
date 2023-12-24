396989c1b579025cf419d5ca8ce2db13c4153a2b
grishingrishin <grishingrishin@yahoo.com>
2023-12-24 18:13:23 +0300

    Added test for the character list component

---
+++ src/components/__test__/character-list.test.js
@@ -0,0 +1,68 @@
+import { describe, test, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+
+import CharacterList from '@/components/CharacterList.vue';
+
+describe('CharacterList', () => {
+	const mockArr = [
+		[
+			{
+				id: 1,
+				picture: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+				name: 'Rick Sanchez',
+				status: 'Alive',
+				gender: 'Male',
+				episodes: ['https://rickandmortyapi.com/api/episode/1'],
+			},
+			{
+				id: 2,
+				picture: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+				name: 'Morty Smith',
+				status: 'Alive',
+				gender: 'Male',
+				episodes: ['https://rickandmortyapi.com/api/episode/1'],
+			},
+			{
+				id: 3,
+				picture: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+				name: 'Summer Smith',
+				status: 'Alive',
+				gender: 'Female',
+				episodes: ['https://rickandmortyapi.com/api/episode/1'],
+			},
+		],
+	];
+
+	test('Mounted pure component', () => {
+		expect(CharacterList).toBeTruthy();
+
+		const wrapper = mount(CharacterList);
+
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	test('Mounted component with character', () => {
+		expect(CharacterList).toBeTruthy();
+
+		const wrapper = mount(CharacterList, {
+			props: {
+				list: mockArr,
+			},
+			shallow: true,
+		});
+
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	test('Mounted component with slot', () => {
+		expect(CharacterList).toBeTruthy();
+
+		const wrapper = mount(CharacterList, {
+			slots: {
+				append: "Hello, i'am slot",
+			},
+		});
+
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+});
