acee288479629328415b179727e52c65dacc2aad
grishingrishin <grishingrishin@yahoo.com>
2024-01-05 18:33:22 +0300

    Refactoring, prettier formatted

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,16 @@ import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: [
-			{
-				find: '@',
-				replacement: path.resolve(__dirname, 'src'),
-			},
-		],
-	},
-	test: {
-		environment: 'happy-dom',
-	},
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
+  test: {
+    environment: 'happy-dom',
+  },
 });
